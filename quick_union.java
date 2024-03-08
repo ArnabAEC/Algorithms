df61e1f1022bce88653e92458fbe548ae3c73b47
ArnabAEC <noonevs@gmail.com>
2024-03-09 01:57:06 +0530

    weighted root update

--- quick_union.java
+++ quick_union.java
@@ -1,12 +1,14 @@
 public class quick_union
 {
     private int[] id;
+    private int[] sz;
 
     public quick_union(int N){
         id = new int[N];
-
+        sz = new int[N];
         for(int i = 0; i < N; i++){
             id[i] = i;
+            sz[i] = 1;
         }
     }
 
@@ -25,13 +27,14 @@ public class quick_union
     public void union(int p, int q){
         int i = root(p);
         int j = root(q);
-        id[j] = i;
+        if(sz[i]<sz[j]) { id[i] = j; sz[j]+=sz[i];} //WEIGHTED ROOT UPDATE
+        else { id[j] = i; sz[j]+=sz[i];}
     }
 
     public static void main(String[] args) {
         quick_union obj = new quick_union(10);
         obj.union(1, 2);
         obj.union(1, 4);
-        System.out.println(obj.connected(2, 4));
+        System.out.println(obj.connected(2, 3));
     }
 }
